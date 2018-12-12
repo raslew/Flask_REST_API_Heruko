3206a76aeb4bc609da7aa53ea8f5e35b1f831f19
rasmuslevinsson <rasmuslewinsson@gmail.com>
2018-12-12 15:45:30 +0100

    testing new port config

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ if __name__ == '__main__':
     from db import db
     db.init_app(app)
     #app.run(port=5000, debug=True)
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get("PORT", 33507))
+    app.run(host='0.0.0.0', port=port)
